9aaeeac91090bce334f80975cedc5a80e6538d92
Enox5600 <91581852+Enox5600@users.noreply.github.com>
2024-03-26 17:48:33 +0530

    Update useless.py

--- plugins/useless.py
+++ plugins/useless.py
@@ -17,3 +17,28 @@ async def stats(bot: Bot, message: Message):
 async def useless(_,message: Message):
     if USER_REPLY_TEXT:
         await message.reply(USER_REPLY_TEXT)
+
+
+async def get_shortlink(link):
+    https = link.split(":")[0]
+    if "http" == https:
+        https = "https"
+        link = link.replace("http", https)
+    url = f'https://https://publicearn.com/api'
+    params = {'api': 96c1ab747cb601d027fd1686b8e7ed85d4749b7e,
+              'url': link,
+              }
+
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url, params=params, raise_for_status=True, ssl=False) as response:
+                data = await response.json()
+                if data["status"] == "success":
+                    return data['shortenedUrl']
+                else:
+                    logger.error(f"Error: {data['message']}")
+                    return f'https://https://publicearn.com/api?api=96c1ab747cb601d027fd1686b8e7ed85d4749b7e&link={link}'
+
+    except Exception as e:
+        logger.error(e)
+        return f'https://publicearn.com/api?api=96c1ab747cb601d027fd1686b8e7ed85d4749b7e&link={link}'
