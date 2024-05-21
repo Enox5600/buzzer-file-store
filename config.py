01a9ff0db5aff1f29eeaf69c10d815e3bc0c580b
Enox5600 <91581852+Enox5600@users.noreply.github.com>
2024-05-21 16:09:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -80,6 +80,19 @@ logging.basicConfig(
 )
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
+# Online Stream and Download
+STREAM_MODE = bool(environ.get('STREAM_MODE', True)) # Set True or Flase
+
+# If Stream Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
+MULTI_CLIENT = False
+SLEEP_THRESHOLD = int(environ.get('SLEEP_THRESHOLD', '60'))
+PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
+if 'DYNO' in environ:
+    ON_HEROKU = True
+else:
+    ON_HEROKU = False
+URL = environ.get("URL", "https://mytestvj-998c9929dc7a.herokuapp.com/")
+
 
 def LOGGER(name: str) -> logging.Logger:
     return logging.getLogger(name)
